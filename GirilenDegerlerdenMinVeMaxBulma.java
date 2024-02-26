faf504b12865bef71f08dbd25b090218bfa49b59
Sedat Ay <159232350+SedatAy1@users.noreply.github.com>
2024-02-26 09:41:31 +0300

    GirilenDegerlerdenMinVeMaxBulma
    GirilenDegerlerdenMinVeMaxBulma

---
+++ GirilenDegerlerdenMinVeMaxBulma.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class GirilenDegerlerdenMinVeMaxBulma {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Kaç tane sayı gireceksiniz: ");
+        int n = scanner.nextInt();
+        ArrayList<Integer> numbers = new ArrayList();
+
+        int min;
+        int max;
+        for (min = 1; min <= n; ++min) {
+            System.out.print("(" + min + ")Sayıyı giriniz : ");
+            max = scanner.nextInt();
+            numbers.add(max);
+        }
+
+        min = (Integer) Collections.min(numbers);
+        max = (Integer) Collections.max(numbers);
+        System.out.println("En Küçük Sayı: " + min + "\nEn Büyük Sayı: " + max);
+
+    }
+}
